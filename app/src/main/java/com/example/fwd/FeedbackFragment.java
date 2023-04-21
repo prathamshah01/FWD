4c4d330578c0f7c078b0f232476ff40bfa04c0fe
prathamshah01 <imprathamshah9@gmail.com>
2023-04-21 16:39:01 +0530

    Share Fragment

--- app/src/main/java/com/example/fwd/FeedbackFragment.java
+++ app/src/main/java/com/example/fwd/FeedbackFragment.java
@@ -8,10 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.fwd.databinding.FragmentFeedbackBinding;
 
-public class HistoryFragment extends Fragment {
+public class FeedbackFragment extends Fragment {
 
-    public HistoryFragment() {
+    private FragmentFeedbackBinding binding;
+
+
+    public FeedbackFragment() {
         // Required empty public constructor
     }
 
@@ -25,7 +29,9 @@ public class HistoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+
+        binding = FragmentFeedbackBinding.inflate(inflater, container ,false);
+        return binding.getRoot();
+
     }
 }
--- app/src/main/java/com/example/fwd/NavDrawerLayout.java
+++ app/src/main/java/com/example/fwd/NavDrawerLayout.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -59,10 +60,16 @@ public class NavDrawerLayout extends AppCompatActivity {
 
 
                 } else if (id == R.id.navFeedback) {
-                    Toast.makeText(NavDrawerLayout.this, "Baki che...", Toast.LENGTH_SHORT).show();
+                    loadFragment(new FeedbackFragment());
 
                 } else if (id == R.id.navRecommend) {
-                    loadFragment(new ShareFragment());
+//                    loadFragment(new ShareFragment());
+
+                    Intent sendIntent = new Intent();
+                    sendIntent.setAction(Intent.ACTION_SEND);
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, "Food Shouldn't  be wasted");
+                    sendIntent.setType("text/plain");
+                    startActivity(sendIntent);
 
 
                 } else if (id == R.id.navSettings) {
